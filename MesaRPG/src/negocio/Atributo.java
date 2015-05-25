a084a6cfdaefde392277ec3d912101d058b67134
Arlindo Jr <ba.elias@gmail.com>
2015-05-25 15:42:43 -0300

    Add/edit - Atributos, Objeto, Raça e Item

---
+++ MesaRPG/src/negocio/Atributo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package negocio;
+
+/**
+ *
+ * @author Arlindo
+ */
+public class Atributo {
+    private int valor;
+    private String nome, descricao;
+
+    public Atributo(int valor, String nome, String descricao) {
+        this.valor = valor;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+       
+    
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
