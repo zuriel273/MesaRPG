d8f9cfccd85fb298e5214e292abba8c2dce7fbb9
Jotave <www.jotave@gmail.com>
2015-06-03 14:55:46 -0300

    Finalizando a classe Dado

--- MesaRPG/src/mesa/Dado.java
+++ MesaRPG/src/mesa/Dado.java
@@ -10,8 +10,19 @@ package mesa;
  * @author JoãoVitor
  */
 import java.util.Random;
+import java.util.Arrays;
 
 public class Dado {
+    
+    public static Dado self;
+    
+    protected Dado(){
+        self = this;
+    }
+    
+    public Dado getInst(){
+        return self;
+    }
     
     public int rodarDados(int lados, int qtd){
         int soma = 0;
@@ -41,6 +52,16 @@ public class Dado {
     }
     
     public int rodarDados(String tipo){
-        return 0;
+        int lados, qtd;
+        
+        try {
+            qtd = Integer.parseInt(String.valueOf(tipo.charAt(0)));
+            lados = Integer.parseInt(String.valueOf(tipo.charAt(2)));
+        } catch (Exception e) {
+            System.out.println("ERROR: Dtring passada no formato invalido");
+            return -1;
+        }
+        
+        return rodarDados(qtd, lados);
     }
 }
